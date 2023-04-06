e4bd0bf772556a0842d94dbe818bd98046e7da3b
Fadel Megahed <fmegahed@users.noreply.huggingface.co>
2023-04-06 15:42:38 +0000

    Add manuscript information to the app

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ iface = gr.Interface(
     outputs = 'text',
     live=True,
     title = "Predicting In-Hospital Mortality After TAVR Using Preoperative Variables and Penalized Logistic Regression",
-    description = "The app below utilizes the finalized logistic regression model with an l2 penalty based on the manuscript by Alhwiti et al. The manuscript will be submitted to JACC: Cardiovascular Interventions. The data used for model building is all TAVR procedures between 2012 and 2019 as reported in the HCUP NIS database. <br><br> The purpose of the app is to provide evidence-based clinical support for interventional cardiology. </b>.",
+    description = "The app below utilizes the finalized logistic regression model with an l2 penalty based on the manuscript by Alhwiti et al(2023). The manuscript is under review at Scientific Reports. The data used for model building is all TAVR procedures between 2012 and 2019 as reported in the HCUP NIS database. <br><br> The purpose of the app is to provide evidence-based clinical support for interventional cardiology. </b>.",
     css = 'https://bootswatch.com/5/journal/bootstrap.css')
 
 iface.launch()
